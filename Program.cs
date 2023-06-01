93030327ecbaa9b71840a17bbbbcf8b3c806228e
rorikwong <108846354+rorikwong@users.noreply.github.com>
2023-05-31 19:09:19 -0500

    changes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 using BlazingPizza.Data;
+using BlazingPizza.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<OrderState>();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
 builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
